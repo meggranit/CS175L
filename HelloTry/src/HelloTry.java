bd18935001c84a7eaee88f32d776e916ee4465c8
meghangranit <meghangranit@10.0.0.160>
2020-09-19 01:39:45 -0400

    q

--- HelloTry/src/HelloTry.java
+++ HelloTry/src/HelloTry.java
@@ -1,8 +1,20 @@
-
+import java.util.Scanner; 
 public class HelloTry {
 
 	public static void main(String[] args) {
-		System.out.print("Hello");
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please enter a building width");
+		int buildingWidth= in.nextInt();
+		System.out.println("Please enter a building height");
+		int buildingHeight= in.nextInt();
+		//int buildingWidth= 50;
+		//int buildingHeight= 30;
+		//boolean yesOrNo= false;
+		
+		
+		//System.out.print("Hello");
+		int sqFt= (buildingWidth*buildingHeight);
+		System.out.println("The square footage is "+sqFt);
 			
 		// TODO Auto-generated method stub
 
